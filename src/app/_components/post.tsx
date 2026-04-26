4bda812698f8ff138be785121eaf1725dd62675d
anticore691 <m.yosh691@gmail.com>
2026-04-26 18:09:16 +0700

    fix: total hapus komponen post

--- src/app/_components/post.tsx
+++ src/app/_components/post.tsx
@@ -1,27 +1,3 @@
-"use client";
-
-import { useState } from "react";
-
-// import { api } from "~/trpc/react";
-
 export function LatestPost() {
-	// The post router was removed, commenting out to fix build errors.
-	/*
-	const [latestPost] = api.post.getLatest.useSuspenseQuery();
-
-	const utils = api.useUtils();
-	const [name, setName] = useState("");
-	const createPost = api.post.create.useMutation({
-		onSuccess: async () => {
-			await utils.post.invalidate();
-			setName("");
-		},
-	});
-	*/
-
-	return (
-		<div className="w-full max-w-xs">
-			<p>Post component disabled.</p>
-		</div>
-	);
+	return null;
 }
